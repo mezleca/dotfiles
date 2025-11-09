c387d449de8616b4ca40f532ada23d5caa55a173
mezleca <mtksenpai@gmail.com>
2025-11-08 21:48:05 -0300

    fix install script

--- install.sh
+++ install.sh
@@ -3,27 +3,32 @@
 DEST="$HOME"
 DOT_FOLDER="./home/"
 
-echo $DOT_FOLDER
-
 echo "adding perms..."
 
 # ensure perms
 for script in $(find . -iname "*.sh"); do
-    chmod +x $script
+    chmod +x "$script"
 done
-
-# copy shit
 
 echo "copying files..."
 
-for base_dir in $(find $DOT_FOLDER); do
-    # ignore symbolic links
-    [ -L "$base_dir" ] && continue
+# create directories
+find "$DOT_FOLDER" -type d | while read -r dir; do
+    REL="${dir#$DOT_FOLDER/}"
+    # skip root folder
+    [ "$REL" = "$DOT_FOLDER" ] && continue
+    
+    TARGET="$HOME/$REL"
+    [ ! -d "$TARGET" ] && mkdir -p "$TARGET"
+done
 
-    REL=$(echo "$base_dir" | awk -v sep="$DOT_FOLDER" '{split($0, arr, sep); print arr[2]}')
-    FINAl="$HOME/$REL"
-
-    cp -r "$base_dir" "$HOME" >/dev/null 2>&1
+# copy files
+find "$DOT_FOLDER" -type f | while read -r file; do
+    REL="${file#$DOT_FOLDER/}"
+    TARGET="$HOME/$REL"
+    
+    echo "copying: $file -> $TARGET"
+    cp "$file" "$TARGET"
 done
 
 echo "done"
