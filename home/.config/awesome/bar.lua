4265b0bdd5e443386ab8c67b433928daf721f834
mezleca <mtksenpai@gmail.com>
2026-01-20 12:51:56 -0300

    use wibox instead of polybar on awesome, remove hacks for auto-maximize (didn't work anyways...) and polybar

---
+++ home/.config/awesome/bar.lua
@@ -0,0 +1,264 @@
+local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
+
+local bar = {}
+
+local REFRESH_INTERVAL_SEC = 2
+local VOLUME_STEP = "10%"
+local SYSTRAY_ICON_SIZE = 16
+local SYSTRAY_MARGIN_PX = 8
+local SYSTRAY_VERTICAL_MARGIN = 7
+local ROUNDED_CORNER_RADIUS = 6
+local TITLE_MAX_LENGTH = 50
+local TAGLIST_MIN_WORKSPACES = 3
+
+-- workspace icons
+local WORKSPACE_ICONS = { "", "", "", "", "", "" }
+
+local function create_separator()
+    return wibox.widget {
+        markup = "<span foreground='" .. beautiful.color_separator .. "'>|</span>",
+        font = beautiful.font,
+        widget = wibox.widget.textbox
+    }
+end
+
+local function create_cpu_widget()
+    local cpu_widget = wibox.widget {
+        markup = "<span foreground='" .. beautiful.color_label .. "'>󰻠  0%</span>",
+        font = beautiful.font,
+        widget = wibox.widget.textbox
+    }
+    
+    awful.widget.watch("sh -c \"top -bn1 | grep 'Cpu(s)' | awk '{print $2}' | cut -d'%' -f1\"", REFRESH_INTERVAL_SEC, function(widget, stdout)
+        local cpu = tonumber(stdout) or 0
+        widget.markup = string.format("<span foreground='%s'>󰻠  %.0f%%</span>", beautiful.color_label, cpu)
+    end, cpu_widget)
+    
+    return cpu_widget
+end
+
+local function create_memory_widget()
+    local mem_widget = wibox.widget {
+        markup = "<span foreground='" .. beautiful.color_label .. "'>󰍛  0%</span>",
+        font = beautiful.font,
+        widget = wibox.widget.textbox
+    }
+    
+    awful.widget.watch("sh -c \"free | grep Mem | awk '{print ($3/$2) * 100.0}'\"", REFRESH_INTERVAL_SEC, function(widget, stdout)
+        local mem = tonumber(stdout) or 0
+        widget.markup = string.format("<span foreground='%s'>󰍛  %.0f%%</span>", beautiful.color_label, mem)
+    end, mem_widget)
+    
+    return mem_widget
+end
+
+local function create_volume_widget()
+    local vol_widget = wibox.widget {
+        markup = "<span foreground='" .. beautiful.color_label .. "'>󰕾  0%</span>",
+        font = beautiful.font,
+        widget = wibox.widget.textbox
+    }
+    
+    local function update_volume()
+        awful.spawn.easy_async_with_shell("pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\\d+%' | head -1", function(stdout)
+            local vol = stdout:match("(%d+)%%") or "0"
+            
+            awful.spawn.easy_async_with_shell("pactl get-sink-mute @DEFAULT_SINK@", function(mute_out)
+                local is_muted = mute_out:match("yes") ~= nil
+                local icon = is_muted and "󰖁" or "󰕾"
+                vol_widget.markup = string.format("<span foreground='%s'>%s  %s%%</span>", beautiful.color_label, icon, vol)
+            end)
+        end)
+    end
+    
+    update_volume()
+    gears.timer {
+        timeout = 0.5,
+        autostart = true,
+        callback = update_volume
+    }
+    
+    vol_widget:buttons(gears.table.join(
+        awful.button({}, 1, function()
+            awful.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")
+            gears.timer.start_new(0.1, function() update_volume() return false end)
+        end),
+        awful.button({}, 3, function()
+            awful.spawn("pavucontrol")
+        end),
+        awful.button({}, 4, function()
+            awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ +" .. VOLUME_STEP)
+            gears.timer.start_new(0.1, function() update_volume() return false end)
+        end),
+        awful.button({}, 5, function()
+            awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ -" .. VOLUME_STEP)
+            gears.timer.start_new(0.1, function() update_volume() return false end)
+        end)
+    ))
+    
+    return vol_widget
+end
+
+local function create_date_widget()
+    return wibox.widget {
+        format = "<span foreground='" .. beautiful.color_label .. "'>󰥔  %H:%M</span>",
+        font = beautiful.font,
+        widget = wibox.widget.textclock
+    }
+end
+
+function bar.create(s)
+    -- custom filter to show min X workspaces, only occupied ones
+    local function taglist_filter(t)
+        local screen_tags = s.tags
+        local last_occupied = 0
+        
+        for i, tag in ipairs(screen_tags) do
+            if #tag:clients() > 0 or tag.selected then
+                last_occupied = i
+            end
+        end
+        
+        local show_until = math.max(TAGLIST_MIN_WORKSPACES, last_occupied)
+        return t.index <= show_until
+    end
+    
+    -- taglist widget
+    local taglist = awful.widget.taglist {
+        screen = s,
+        filter = taglist_filter,
+        buttons = gears.table.join(
+            awful.button({}, 1, function(t) t:view_only() end),
+            awful.button({ MODKEY }, 1, function(t)
+                if client.focus then
+                    client.focus:move_to_tag(t)
+                end
+            end),
+            awful.button({}, 4, function(t) awful.tag.viewnext(t.screen) end),
+            awful.button({}, 5, function(t) awful.tag.viewprev(t.screen) end)
+        ),
+        widget_template = {
+            {
+                {
+                    id = 'text_role',
+                    widget = wibox.widget.textbox,
+                },
+                left = 8,
+                right = 8,
+                widget = wibox.container.margin
+            },
+            id = 'background_role',
+            widget = wibox.container.background,
+            create_callback = function(self, tag, index, objects)
+                local icon_index = index <= #WORKSPACE_ICONS and index or #WORKSPACE_ICONS
+                self:get_children_by_id('text_role')[1].markup = 
+                    "<span foreground='" .. beautiful.color_inactive .. "'>" .. WORKSPACE_ICONS[icon_index] .. "</span>"
+                self:get_children_by_id('text_role')[1].font = beautiful.taglist_font
+            end,
+            update_callback = function(self, tag, index, objects)
+                local icon_index = index <= #WORKSPACE_ICONS and index or #WORKSPACE_ICONS
+                local color = beautiful.color_inactive
+                
+                if tag.selected then
+                    color = beautiful.color_active
+                elseif #tag:clients() > 0 then
+                    color = beautiful.color_accent
+                end
+                
+                self:get_children_by_id('text_role')[1].markup = 
+                    "<span foreground='" .. color .. "'>" .. WORKSPACE_ICONS[icon_index] .. "</span>"
+            end
+        }
+    }
+    
+    -- window title widget
+    local window_title = wibox.widget {
+        markup = "<span foreground='" .. beautiful.color_label .. "'>Desktop</span>",
+        font = beautiful.font,
+        widget = wibox.widget.textbox
+    }
+    
+    local function update_title(c)
+        local title = (c and c.name) or "Desktop"
+        if #title > TITLE_MAX_LENGTH then
+            title = title:sub(1, TITLE_MAX_LENGTH - 3) .. "..."
+        end
+        window_title.markup = "<span foreground='" .. beautiful.color_label .. "'>" .. gears.string.xml_escape(title) .. "</span>"
+    end
+
+    client.connect_signal("focus", function(c) update_title(c) end)
+    client.connect_signal("unfocus", function() if not client.focus then update_title() end end)
+    client.connect_signal("property::name", function(c) if client.focus == c then update_title(c) end end)
+    
+    -- systray with rounded box
+    local systray = wibox.widget.systray()
+    systray:set_base_size(SYSTRAY_ICON_SIZE)
+    
+    local systray_container = wibox.widget {
+        {
+            {
+                {
+                    systray,
+                    left = SYSTRAY_MARGIN_PX,
+                    right = SYSTRAY_MARGIN_PX,
+                    widget = wibox.container.margin
+                },
+                valign = "center",
+                widget = wibox.container.place
+            },
+            bg = beautiful.bg_systray,
+            shape = function(cr, width, height)
+                gears.shape.rounded_rect(cr, width, height, ROUNDED_CORNER_RADIUS)
+            end,
+            widget = wibox.container.background
+        },
+        top = SYSTRAY_VERTICAL_MARGIN,
+        bottom = SYSTRAY_VERTICAL_MARGIN,
+        widget = wibox.container.margin
+    }
+    
+    -- create the wibar
+    s.mywibox = awful.wibar({
+        position = "bottom",
+        screen = s,
+        height = beautiful.wibar_height,
+        bg = beautiful.wibar_bg,
+        fg = beautiful.wibar_fg,
+        ontop = false,
+        restrict_workarea = true
+    })
+    
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        {
+            layout = wibox.layout.fixed.horizontal,
+            spacing = 10,
+            {
+                widget = wibox.container.margin,
+                left = 5,
+                taglist
+            },
+            create_separator(),
+            window_title
+        },
+        nil,
+        {
+            layout = wibox.layout.fixed.horizontal,
+            spacing = 10,
+            systray_container,
+            create_cpu_widget(),
+            create_memory_widget(),
+            create_volume_widget(),
+            create_date_widget(),
+            {
+                widget = wibox.container.margin,
+                right = 10
+            }
+        }
+    }
+end
+
+return bar
--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -2,29 +2,25 @@ local gears = require("gears")
 local awful = require("awful")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
+local bar = require("bar")
 
 require("awful.autofocus")
 
-local FULLSCREEN_THRESHOLD = 0.9
-local SANE_SIZE_RATIO = 0.75
+MODKEY = "Mod4"
+ALTKEY = "Mod1"
 
-modkey = "Mod4"
-altkey = "Mod1"
-
-terminal     = "kitty"
-file_manager = "nemo"
-launcher     = os.getenv("HOME") .. "/.config/rofi/scripts/launcher.sh"
-power_menu   = os.getenv("HOME") .. "/.config/rofi/scripts/power_menu.sh"
+TERMINAL     = "kitty"
+FILE_MANAGER = "nemo"
+LAUNCHER     = os.getenv("HOME") .. "/.config/rofi/scripts/launcher.sh"
+POWER_MENU   = os.getenv("HOME") .. "/.config/rofi/scripts/power_menu.sh"
 
 -- setup theme
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.border_width = 3
-beautiful.border_focus = "#3d466b"
-beautiful.border_normal = "#333333"
-
+beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 awful.layout.layouts = { awful.layout.suit.floating }
 
-local function autostart(cmd) awful.spawn.with_shell(cmd) end
+local function autostart(cmd) 
+    awful.spawn.with_shell(cmd) 
+end
 
 -- autostart apps
 autostart("otd-daemon")
@@ -32,7 +28,6 @@ autostart("dunst")
 autostart("picom")
 autostart("dex --autostart --environment awesome")
 autostart("nm-applet")
-autostart("~/.config/polybar/launch.sh")
 autostart("feh --bg-fill ~/wallpapers/7.jpg")
 
 awful.spawn.with_shell([[
@@ -56,34 +51,8 @@ naughty.connect_signal("request::display_error", function(message, startup)
     ))
 end)
 
-local polybar_hidden = false
-
-local function update_polybar()
-    local tag = awful.screen.focused().selected_tag
-    if not tag then return end
-
-    local should_hide = false
-    for _, c in ipairs(tag:clients()) do
-        if c.fullscreen and c:isvisible() then
-            should_hide = true
-            break
-        end
-    end
-
-    if should_hide and not polybar_hidden then
-        awful.spawn.with_shell("polybar-msg cmd hide")
-        polybar_hidden = true
-    elseif not should_hide and polybar_hidden then
-        awful.spawn.with_shell("polybar-msg cmd show")
-        polybar_hidden = false
-    end
-end
-
 -- apps that must always be fullscreen (forced once on startup)
-local force_fullscreen = { "osu!%.exe", "steam_app_" }
-
--- browsers have weird EWMH bugs, let them manage their own state
-local ignore_state_management = { "firefox", "chromium", "chrome", "brave" }
+local FORCE_FULLSCREEN = { "osu!%.exe", "steam_app_" }
 
 local function class_matches(c, patterns)
     if not c or not c.class then return false end
@@ -95,64 +64,54 @@ local function class_matches(c, patterns)
 end
 
 local function should_be_fullscreen(c)
-    return class_matches(c, force_fullscreen)
-end
-
-local function should_ignore_state(c)
-    return class_matches(c, ignore_state_management)
-end
-
-local function should_maximize_by_size(c)
-    if not c or not c.valid or not c.screen then return false end
-    local wa = c.screen.workarea
-    local g = c:geometry()
-    return (g.width / wa.width >= FULLSCREEN_THRESHOLD) and
-           (g.height / wa.height >= FULLSCREEN_THRESHOLD)
-end
-
-local function normalize_geometry(c)
-    if not c or not c.valid or not c.screen then return end
-    local wa = c.screen.workarea
-    local w = math.floor(wa.width * SANE_SIZE_RATIO)
-    local h = math.floor(wa.height * SANE_SIZE_RATIO)
-    c:geometry({
-        x = wa.x + math.floor((wa.width - w) / 2),
-        y = wa.y + math.floor((wa.height - h) / 2),
-        width = w, height = h
-    })
+    return class_matches(c, FORCE_FULLSCREEN)
 end
 
 -- mouse binds
-local clientbuttons = gears.table.join(
+local client_buttons = gears.table.join(
     awful.button({}, 1, function(c) c:activate({ context = "mouse_click", raise = true }) end),
-    awful.button({ modkey }, 1, function(c) c:activate({ context = "mouse_click" }); awful.mouse.client.move(c) end),
-    awful.button({ modkey }, 3, function(c) c:activate({ context = "mouse_click" }); awful.mouse.client.resize(c) end)
+    awful.button({ MODKEY }, 1, function(c) 
+        c:activate({ context = "mouse_click" })
+        awful.mouse.client.move(c) 
+    end),
+    awful.button({ MODKEY }, 3, function(c) 
+        c:activate({ context = "mouse_click" })
+        awful.mouse.client.resize(c) 
+    end)
 )
 
 -- keybindings
-local globalkeys = gears.table.join(
-    awful.key({ modkey }, "Return", function() awful.spawn(terminal) end),
-    awful.key({ modkey }, "e", function() awful.spawn(file_manager) end),
-    awful.key({ modkey }, "d", function() awful.spawn.with_shell(launcher) end),
-    awful.key({ modkey }, "p", function() awful.spawn.with_shell(power_menu) end),
-    awful.key({ modkey, "Shift" }, "r", awesome.restart),
+local global_keys = gears.table.join(
+    awful.key({ MODKEY }, "Return", function() awful.spawn(TERMINAL) end),
+    awful.key({ MODKEY }, "e", function() awful.spawn(FILE_MANAGER) end),
+    awful.key({ MODKEY }, "d", function() awful.spawn.with_shell(LAUNCHER) end),
+    awful.key({ MODKEY }, "p", function() awful.spawn.with_shell(POWER_MENU) end),
+    awful.key({ MODKEY, "Shift" }, "r", awesome.restart),
 
-    awful.key({ modkey }, "q", function() if client.focus then client.focus:kill() end end),
-
-    awful.key({ modkey }, "f", function()
-        if client.focus then client.focus.maximized = not client.focus.maximized; client.focus:raise() end
+    awful.key({ MODKEY }, "q", function() 
+        if client.focus then client.focus:kill() end 
     end),
 
-    awful.key({ modkey, "Shift" }, "f", function()
-        if client.focus then client.focus.fullscreen = not client.focus.fullscreen; client.focus:raise() end
+    awful.key({ MODKEY }, "f", function()
+        if client.focus then 
+            client.focus.maximized = not client.focus.maximized
+            client.focus:raise() 
+        end
     end),
 
-    awful.key({ altkey }, "Tab", function()
+    awful.key({ MODKEY, "Shift" }, "f", function()
+        if client.focus then 
+            client.focus.fullscreen = not client.focus.fullscreen
+            client.focus:raise() 
+        end
+    end),
+
+    awful.key({ ALTKEY }, "Tab", function()
         awful.client.focus.byidx(1)
         if client.focus then client.focus:raise() end
     end),
 
-    awful.key({ altkey, "Shift" }, "Tab", function()
+    awful.key({ ALTKEY, "Shift" }, "Tab", function()
         awful.client.focus.byidx(-1)
         if client.focus then client.focus:raise() end
     end),
@@ -164,18 +123,19 @@ local globalkeys = gears.table.join(
     awful.key({}, "XF86AudioMicMute", function() awful.spawn("pactl set-source-mute @DEFAULT_SOURCE@ toggle") end)
 )
 
--- setup workspaces
+-- setup workspaces and bar
 awful.screen.connect_for_each_screen(function(s)
-    awful.tag({ "1","2","3","4","5","6","7","8","9","10" }, s, awful.layout.suit.floating)
+    awful.tag({ "1","2","3","4","5" }, s, awful.layout.suit.floating)
+    bar.create(s)
 end)
 
-for i = 1, 10 do
-    globalkeys = gears.table.join(globalkeys,
-        awful.key({ modkey }, "#" .. i + 9, function()
+for i = 1, 5 do
+    global_keys = gears.table.join(global_keys,
+        awful.key({ MODKEY }, "#" .. i + 9, function()
             local tag = awful.screen.focused().tags[i]
             if tag then tag:view_only() end
         end),
-        awful.key({ modkey, "Shift" }, "#" .. i + 9, function()
+        awful.key({ MODKEY, "Shift" }, "#" .. i + 9, function()
             if client.focus then
                 local tag = client.focus.screen.tags[i]
                 if tag then client.focus:move_to_tag(tag) end
@@ -184,7 +144,7 @@ for i = 1, 10 do
     )
 end
 
-root.keys(globalkeys)
+root.keys(global_keys)
 
 -- default rules
 awful.rules.rules = {
@@ -199,13 +159,9 @@ awful.rules.rules = {
             floating = true,
             maximized = false,
             fullscreen = false,
-            buttons = clientbuttons
+            buttons = client_buttons
         }
     },
-    {
-        rule_any = { class = { "Polybar", "polybar" } },
-        properties = { border_width = 0, focusable = false, skip_taskbar = true }
-    }
 }
 
 -- signals
@@ -218,13 +174,7 @@ client.connect_signal("manage", function(c)
     if should_be_fullscreen(c) then
         c.fullscreen = true
         c.floating = false
-        update_polybar()
         return
-    end
-
-    -- auto maximize oversized windows
-    if should_maximize_by_size(c) and not should_ignore_state(c) then
-        c.maximized = true
     end
 end)
 
@@ -238,12 +188,8 @@ end)
 
 client.connect_signal("property::fullscreen", function(c)
     c.border_width = c.fullscreen and 0 or beautiful.border_width
-    update_polybar()
 end)
 
 client.connect_signal("property::maximized", function(c)
     c.border_width = c.maximized and 0 or beautiful.border_width
 end)
-
-tag.connect_signal("property::selected", update_polybar)
-client.connect_signal("unmanage", update_polybar)
